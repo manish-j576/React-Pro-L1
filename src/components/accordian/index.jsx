18773b586d33baa84780a0b7b83d2688e23556c9
Manish Joshi <manishjoshi705572@gmail.com>
2025-04-18 12:33:47 +0530

    multiple Select accordian

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -3,9 +3,29 @@ import data from "./data";
 import "./styles.css";
 export function Accodian() {
   const [selected, setSeleceted] = useState(null);
+  const [enableMulti, setEnableMulti] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
+  function onClickHandler(id) {
+    if (enableMulti) {
+      onMultipleClickHandler(id);
+    } else {
+      onSigleClickhandler(id);
+    }
+  }
+  function onMultipleClickHandler(id) {
+    const copyMul = [...multiple];
+    const index = copyMul.indexOf(id);
+
+    if (index === -1) {
+      copyMul.push(id);
+    } else {
+      copyMul.splice(index, 1);
+    }
+
+    setMultiple(copyMul);
+  }
   function onSigleClickhandler(id) {
-    console.log("single onclick handler");
     setSeleceted(id == selected ? null : id);
   }
 
@@ -15,12 +35,21 @@ export function Accodian() {
         <div className="heading">
           <h1>accordian</h1>
           <div></div>
+          <div>
+            <button onClick={() => setEnableMulti(!enableMulti)}>
+              {enableMulti ? (
+                <h4>Disable Multiselect </h4>
+              ) : (
+                <h4>Enable MultiSelect</h4>
+              )}
+            </button>
+          </div>
         </div>
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div
               onClick={() => {
-                onSigleClickhandler(dataItem.id);
+                onClickHandler(dataItem.id);
               }}
               className="item"
               key={dataItem.id}
@@ -29,9 +58,16 @@ export function Accodian() {
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id ? (
+              {enableMulti
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+              {/* {selected === dataItem.id ? (
                 <div className="content">{dataItem.answer}</div>
-              ) : null}
+              ) : null} */}
             </div>
           ))
         ) : (
